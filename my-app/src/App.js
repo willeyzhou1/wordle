55e62e27d3309880e85fe9a680ea1c6ba1d7d5bc
willersss <willzhou000@gmail.com>
2024-10-18 13:31:31 -0700

    react start

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <a href="https://github.com/willeyzhou1/react-website-portfolio-practice/blob/main/src/App.js">meow meow</a>
+  );
+}
+
+export default App;
